cc26dac99e863ef2726e4cd1bb50b634dc259654
TAdmin <TAdmin@tadmins-mbp.myhome.westell.com>
2014-10-30 10:00:28 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -8,7 +8,7 @@
 {
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
-    /*[self addObstacle];*/
+    [self addObstacle];
     timeSinceObstacle = 0.0f;
     
 
@@ -16,11 +16,11 @@
 
 -(void)update:(CCTime)delta
 {
-    timeSinceObstacle += delta;
+    /*timeSinceObstacle += delta;
     if (timeSinceObstacle> 2.0f) {
         [self addObstacle];
         timeSinceObstacle = 0.0;
-    }
+    }*/
 }
 
 
